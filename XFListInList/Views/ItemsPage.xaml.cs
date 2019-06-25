3da4e6596b683e2194bb555baab1a4a5e2c6a0ad
Michael Heinrich <M.Heinrich@de.bernstein.eu>
2019-06-25 11:50:22 +0200

    Use CollectionView

--- XFListInList/Views/ItemsPage.xaml.cs
+++ XFListInList/Views/ItemsPage.xaml.cs
@@ -24,17 +24,6 @@ namespace XFListInList.Views {
             BindingContext = viewModel = new ItemsViewModel();
         }
 
-        async void OnItemSelected (object sender, SelectedItemChangedEventArgs args) {
-            var item = args.SelectedItem as Item;
-            if (item == null)
-                return;
-
-            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
-
-            // Manually deselect item.
-            ItemsListView.SelectedItem = null;
-        }
-
         async void AddItem_Clicked (object sender, EventArgs e) {
             await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
         }
